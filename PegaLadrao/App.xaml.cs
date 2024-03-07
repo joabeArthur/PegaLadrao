b968a06fc788b672cbfe1885a2ada3d260acc9a5
Joabe <joalokk@gmail.com>
2024-03-07 09:44:03 -0300

    Primeiro Commit (restaurado projeto via Professor)

---
+++ PegaLadrao/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PegaLadrao;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PegaLadrao/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PegaLadrao;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void ClicaeComeca(object sender, EventArgs args)
+	{
+		
+	}
+}
+
